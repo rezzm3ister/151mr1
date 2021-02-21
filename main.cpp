350c436c7d77da9da966311696f26f74e145d9bf
Luis Alvarez <mlpa16.2@gmail.com>
2021-02-21 15:43:20 +0800

    isdouble implementation

--- main.cpp
+++ main.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
+#include <sstream>
 using namespace std;
 
+bool isDouble(string s, double& value){
+  //long double ld;
+  //cout<<(std::istringstream(s) >> value >> std::ws).eof();
+  double temp;
+  bool check=(istringstream(s) >> temp >> ws).eof();
+  if(check==0){
+    return false;
+  }
+  else{
+    value = temp;
+    return true;
+  }
+
+}
+
 int main(){
-  cout<<"hello world"<<endl;
-  cout<<"test v2";
+  string in;
+  double value=0;
+  cout<<"enter string: ";
+  cin>>in;
+
+  cout<<"is "<<in<<" a double? \n0=no, 1=yes"<<endl;
+  cout<<isDouble(in,value)<<endl;
+  if(isDouble(in,value)==1){
+    cout<<"float value: "<<value<<endl;
+  }
+  else{
+    cout<<in<<" is not a double"<<endl;
+  }
+
+
 }
