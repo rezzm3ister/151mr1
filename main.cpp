984d0dfa7ff7828d437bbbab7e54c6af6b92ed55
Luis Alvarez <mlpa16.2@gmail.com>
2021-03-03 21:41:22 +0800

    added check

--- main.cpp
+++ main.cpp
@@ -23,12 +23,15 @@ int main()
   {//empty since its just an entry check
   }
 
+//for testing if it got the whole dataset in, can comment out later
   for(int i=0;i<xdata.size();i-=-1)
   {
+    cout<<"start index: "<<xstart<<endl;
     cout<<xdata[i]<<endl;
   }
   for(int i=0;i<ydata.size();i-=-1)
   {
+    cout<<"start index: "<<ystart<<endl;
     cout<<ydata[i]<<endl;
   }
 
