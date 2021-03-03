004a3288e6d5d1254be8850ad3015beea697b9eb
Luis Alvarez <mlpa16.2@gmail.com>
2021-03-03 19:46:50 +0800

    added headers

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include <vector>
+#include "functions.h"
 using namespace std;
 
 int main(){
-  cout<<"hello world"<<endl;
-  cout<<"test maincpp";
+  //cout<<"hello world"<<endl;
+  //cout<<"test maincpp";
+  vector<double> xdata;
+  vector<double> ydata;
+  datain(xdata);
+
+  
+  
 }
