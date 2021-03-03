43c0d7fea170e34d56e07ce32ff95cb09bbab385
Elijah <elijahtuyay12@gmail.com>
2021-03-03 22:29:57 +0800

    test idk

--- functions.h
+++ functions.h
@@ -4,24 +4,18 @@
 #include <vector>
 #include <fstream>
 #include <sstream>
-<<<<<<< HEAD
-=======
 #include "functions.h"
->>>>>>> 202c84de50d62c6579d11ef9a6133a8af179d2ed
 
 using namespace std;
 bool isDouble(string s, double& value);
 
-<<<<<<< HEAD
 void datain(vector<double> &data)
 {
   string fname;
   cout<<"File Name:";
   getline(fname);
-=======
 bool datain(vector<double> &data69, char dtype, int &start);
 //bool datainv2(vector<double> &data);
->>>>>>> 202c84de50d62c6579d11ef9a6133a8af179d2ed
 
 
 
